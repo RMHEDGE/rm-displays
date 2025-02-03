399b7a671e224a440b2c6ac5ce9070a9d78614dc
flora ~ 🏳️‍⚧️ ❤ 🦀 <35181375+tascord@users.noreply.github.com>
2025-02-04 10:22:10 +1100

    Update display_demo.js

--- display_demo.js
+++ display_demo.js
@@ -1,7 +1,7 @@
 module.exports = {
-    site: "https://google.com",
-    reload: 10 * 60 * 1000,
-    onLoad: () => {
+    "site": "https://google.com",
+    "reload": 10 * 60 * 1000,
+    "onLoad": () => {
         document.querySelector('textarea').value = "WOAH!";
     }
 }
