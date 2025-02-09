f5a11737b43cd27d79c4fd3aefae17472e3d8f37
flora ~ 🏳️‍⚧️ ❤ 🦀 <35181375+tascord@users.noreply.github.com>
2025-02-10 10:19:40 +1100

    Update it_tv.js

--- it_tv.js
+++ it_tv.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "site": "https://turbo.fish/random",
+    "site": "https://sites.apps.rmhedge.com/#app_status?tab=message_board",
     "reload": 600000,
     "onLoad": () => {
         
