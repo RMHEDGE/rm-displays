80100ab1a21c619cf3ab2174cb2506ece3e0e369
flora ~ 🏳️‍⚧️ ❤ 🦀 <35181375+tascord@users.noreply.github.com>
2025-02-10 13:23:51 +1100

    Update craig_tv.js

--- craig_tv.js
+++ craig_tv.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "site": "https://sites.apps.rmhedge.com/#app_status/pdf_view?tab=message_board",
+    "site": "https://sites.apps.rmhedge.com/#app_status/pdf_view?tab=message_board&rotate=true",
     "reload": 600000,
     "onLoad": () => {
         document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale=0.55');
